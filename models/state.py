6209b17696f6aa46720b3f49ee90c66d0e408a47
BekaHabesha <bereketdm1984@gmail.com>
2024-02-22 00:18:17 +0300

    state.py - models file - 0x02. AirBnB clone - MySQL.

--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
-import models
-import shlex
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
@@ -16,8 +16,8 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete,
-                          delete-orphan', backref="state")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
