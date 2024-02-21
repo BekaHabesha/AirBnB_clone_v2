184715d61da2b1b25cdd1f5f33352c710c5e8f3c
BekaHabesha <bereketdm1984@gmail.com>
2024-02-22 00:14:42 +0300

    city.py - models file - 0x02. AirBnB clone - MySQL.

--- models/city.py
+++ models/city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from models.place import Place
 
 
@@ -16,7 +16,6 @@ class City(BaseModel, Base):
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'),
-                      nullable=False)
-    places = relationship("Place", cascade='all, delete,
-                          delete-orphan', backref="cities")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
