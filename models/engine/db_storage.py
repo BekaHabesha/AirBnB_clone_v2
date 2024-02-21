f9b27365185f0e9008ee1a3f66b0431a5fe58da2
BekaHabesha <bereketdm1984@gmail.com>
2024-02-22 00:20:55 +0300

    db_storage.py file - models/engine file - 0x02. AirBnB clone - MySQL.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,13 +4,13 @@ from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
-from models.amenity import Amenity
 from models.base_model import Base
+from models.state import State
 from models.city import City
+from models.user import User
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+from models.amenity import Amenity
 
 
 class DBStorage:
@@ -74,8 +74,7 @@ class DBStorage:
         """configuration
         """
         Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine,
-                           expire_on_commit=False)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
         self.__session = Session()
 
