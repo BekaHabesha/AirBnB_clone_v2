869493022602283eb5fe7508dfd8eed5a2be9219
BekaHabesha <bereketdm1984@gmail.com>
2024-02-22 00:25:52 +0300

    console.py file - 0x02. AirBnB clone - MySQL.

--- console.py
+++ console.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines the HBnB console Module."""
+""" Console Module """
 import cmd
 import sys
-from models.__init__ import storage
-from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from models.__init__ import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
