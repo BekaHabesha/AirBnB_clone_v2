f40573f1063fdfe4f3d8e831ee7b679ab6139851
bekahabesha <139154433+BekaHabesha@users.noreply.github.com>
2024-03-09 15:59:40 -0800

    2-do_deploy_web_static.py - No2.Deploy archive.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,24 @@ from fabric.api import *
 import os
 
 env.hosts = ["52.86.133.162", "54.237.45.210"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+    return the archive path
+    if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
