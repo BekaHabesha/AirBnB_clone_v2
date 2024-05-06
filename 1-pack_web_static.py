bc39f70ce365c28205581066a08177a67d49b491
BekaHabesha <bereketdm1984@gmail.com>
2024-05-06 14:34:28 -0700

    2-do_deploy_web_static.py - No2.Deploy archive.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
-import os
+"""Generate .tgz file from the contents of the web_static folder"""
+
+from fabric import api
 from datetime import datetime
-from fabric.api import local, runs_once
+import os
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-    except Exception:
-        output = None
-    return output
+    """Function to create tarball of webstatic files from the web_static
+    folder in Airbnb_v2.
+
+    Returns: path of .tgz file on success, None otherwise
+    """
+    with api.settings(warn_only=True):
+        isdir = os.path.isdir('versions')
+        if not isdir:
+            mkdir = api.local('mkdir versions')
+            if mkdir.failed:
+                return None
+        suffix = datetime.now().strftime('%Y%m%d%M%S')
+        path = 'versions/web_static_{}.tgz'.format(suffix)
+        tar = api.local('tar -cvzf {} web_static'.format(path))
+        if tar.failed:
+            return None
+        size = os.stat(path).st_size
+        print('web_static packed: {} -> {}Bytes'.format(path, size))
+        return path
